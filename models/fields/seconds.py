45cbdb1a17b40f16ea64c8889ab3b2ee511612b3
Filip Pazera <pazeraf@gmail.com>
2024-10-06 19:29:43 +0200

    Update copyright

--- models/fields/seconds.py
+++ models/fields/seconds.py
@@ -1,6 +1,6 @@
 #
 # Copyright 2012-2014 John Whitlock
-# Modified by Filip Pazera
+# Copyright 2024 Filip Pazera
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
